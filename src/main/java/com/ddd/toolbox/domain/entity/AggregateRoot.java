509b9036329087d85ffbf51d7ad10d55b8cb6a3d
dyywinner <dyywinner@126.com>
2021-02-17 00:28:18 +0800

    finish agg publish event

--- src/main/java/com/ddd/toolbox/domain/entity/AggregateRoot.java
+++ src/main/java/com/ddd/toolbox/domain/entity/AggregateRoot.java
@@ -1,8 +1,10 @@
 package com.ddd.toolbox.domain.entity;
 
 import com.ddd.toolbox.domain.event.BaseDomainEvent;
+import com.ddd.toolbox.domain.event.BaseDomainEventContext;
 import com.ddd.toolbox.domain.event.DefaultLocalDomainPublisher;
 import com.ddd.toolbox.domain.event.IDomainEventPublisher;
+import com.ddd.toolbox.domain.exceptions.EventException;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Resource;
@@ -19,9 +21,47 @@ import java.util.Objects;
 abstract
 public class AggregateRoot extends Entity {
 
-    @Resource
+    @Resource(name = "defaultLocalDomainPublisher")
     private DefaultLocalDomainPublisher defaultLocalDomainPublisher;
 
+    /**
+     * create domain event and publish event
+     * need override createDomainEventWithContext first
+     * @param  domainEventClass  domain event type
+     * @param  context           domain event context
+     * @throws Exception         exception
+     */
+    public void createAndPublishEvent(Class<? extends BaseDomainEvent> domainEventClass, BaseDomainEventContext context)
+            throws Exception {
+        BaseDomainEvent event = domainEventClass.cast(createDomainEventWithContext(domainEventClass, context));
+        if(Objects.isNull(event)) {
+            throw new EventException("create domain event failure");
+        }
+        publishDefault(event);
+    }
+
+    /**
+     * create DomainEvent by args
+     * @param   domainEventClass  domian class tpye
+     * @param   args              params
+     * @return  BaseDomainEvent
+     * @throws  EventException
+     */
+    public BaseDomainEvent createDomainEvent(Class<? extends BaseDomainEvent> domainEventClass, Objects... args)
+            throws EventException {
+        throw new EventException("Aggregate need override method[createDomainEvent]");
+    }
+
+    /**
+     * create domain event from context
+     * @param   domainEventClass  domain event type
+     * @param   context           domain context
+     * @return  BaseDomainEvent
+     */
+    public BaseDomainEvent createDomainEventWithContext(Class<? extends BaseDomainEvent> domainEventClass,
+                                                        BaseDomainEventContext context) {
+        throw new EventException("Aggregate need override method[createDomainEvent]");
+    }
 
     public void publishEvent(List<BaseDomainEvent> events) throws Exception {
         for(BaseDomainEvent event : events) {
@@ -37,6 +77,23 @@ public class AggregateRoot extends Entity {
         IDomainEventPublisher udfPublisher = event.customDomainEventPublisher();
         if(Objects.nonNull(udfPublisher)) {
             udfPublisher.publish(event);
+        }
+    }
+
+    /**
+     * default event publish method
+     * @param  event        domain event
+     * @throws Exception    exception
+     */
+    public void publishDefault(BaseDomainEvent event) throws Exception {
+        if(Objects.isNull(event)) {
+            return;
+        }
+        IDomainEventPublisher udfPublisher = event.customDomainEventPublisher();
+        if(Objects.nonNull(udfPublisher)) {
+            udfPublisher.publish(event);
+        } else {
+            defaultLocalDomainPublisher.publish(event);
         }
     }
 
---
+++ src/main/java/com/ddd/toolbox/domain/event/BaseDomainEventContext.java
@@ -0,0 +1,10 @@
+package com.ddd.toolbox.domain.event;
+
+/**
+ * Event Context
+ * @author daiyuanyang
+ * @date 2021-02-17 00:18
+ **/
+abstract
+public class BaseDomainEventContext {
+}
